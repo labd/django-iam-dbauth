1dd38044b99bc5c28b8570c404857fad8630ef5d
Mike Dingjan <m.dingjan@labdigital.nl>
2020-11-04 09:34:54 +0100

    Resolve formatting errors

--- src/django_iam_dbauth/aws/postgresql/base.py
+++ src/django_iam_dbauth/aws/postgresql/base.py
@@ -9,11 +9,11 @@ from django_iam_dbauth.utils import resolve_cname
 class DatabaseWrapper(base.DatabaseWrapper):
     def get_connection_params(self):
         params = super().get_connection_params()
-        enabled = params.pop('use_iam_auth', None)
+        enabled = params.pop("use_iam_auth", None)
         if enabled:
             rds_client = boto3.client("rds")
 
-            hostname = params.get('host')
+            hostname = params.get("host")
             hostname = resolve_cname(hostname) if hostname else "localhost"
 
             params["password"] = rds_client.generate_db_auth_token(
